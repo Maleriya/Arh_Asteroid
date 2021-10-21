a0a17850108b02076dd1eeb1edfaa973f64a9871
Maleriya <50017431+Maleriya@users.noreply.github.com>
2021-10-21 18:36:41 +0300

    ChainOfResponsibility_Command_Memento

---
+++ Assets/Scripts/Command/BaseUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Asteroids.Command
+{
+    internal abstract class BaseUI : MonoBehaviour
+    {
+        public abstract void Execute();
+        public abstract void Cancel();
+    }
+}
---
+++ Assets/Scripts/Modifier/AddAttackModifier.cs
@@ -0,0 +1,19 @@
+﻿namespace Asteroids.Modifier
+{
+    internal sealed class AddAttackModifier : SimpleWeaponModifier
+    {
+        private readonly int _attack;
+
+        public AddAttackModifier(SimpleWeapon simpleWeapon, int attack)
+            :base(simpleWeapon)
+        {
+            _attack = attack;
+        }
+
+        public override void Handle()
+        {
+            _simpleWeapon.Attack += _attack;
+            base.Handle();
+        }
+    }
+}
